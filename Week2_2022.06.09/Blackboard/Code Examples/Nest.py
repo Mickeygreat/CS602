a9349510b48fd810feffd88a3bbdde1cc31fab2e
Mickey (Chin-Chia) Yeh <98423082+Mickeygreat@users.noreply.github.com>
2022-10-28 03:39:33 -0400

    Delete Week2_2022.06.09 directory

--- Week2_2022.06.09/Blackboard/Code Examples/Nest.py
+++
@@ -1,8 +0,0 @@
-i = 1
-while i <= 3:
-    j = 1
-    while j <= 6:
-        print('*' * j)
-        j += 1
-    print("\n")
-    i += 1
--- Week2_2022.06.09/Shared Files/2022.06.09/SubStrings/String5.py
+++
@@ -1,5 +0,0 @@
-fruit = 'banana'
-length = len(fruit)
-last = fruit[length-1]
-print(last)
-print (fruit[0])
